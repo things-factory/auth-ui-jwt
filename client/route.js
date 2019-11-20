c7cc3cf2ee816a58b5c7a731ecd217fbfa9d579e
Murky <moosung.gil@hatiolab.com>
2019-11-20 11:50:04 +0900

    named chunk and prefetch

--- client/route.js
+++ client/route.js
@@ -1,23 +1,43 @@
 export default function route(page) {
   switch (page) {
     case 'signup':
-      import('./pages/signup.js')
+      import(
+        /* webpackPrefetch: true */
+        /* webpackChunkName: "signup" */
+        './pages/signup.js'
+      )
       return page
 
     case 'signin':
-      import('./pages/signin.js')
+      import(
+        /* webpackPrefetch: true */
+        /* webpackChunkName: "signin" */
+        './pages/signin.js'
+      )
       return page
 
     case 'profile':
-      import('./pages/profile.js')
+      import(
+        /* webpackPrefetch: true */
+        /* webpackChunkName: "profile" */
+        './pages/profile.js'
+      )
       return page
 
     case 'domain-select':
-      import('./pages/domain-select.js')
+      import(
+        /* webpackPrefetch: true */
+        /* webpackChunkName: "domain-select" */
+        './pages/domain-select.js'
+      )
       return page
 
     case 'activate':
-      import('./pages/activate.js')
+      import(
+        /* webpackPrefetch: true */
+        /* webpackChunkName: "activate" */
+        './pages/activate.js'
+      )
       return page
   }
 }
